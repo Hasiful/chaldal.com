ec6bf29b06d091b4067de14dc408ed1cb5aae6e9
Hasiful <hasifulislam32@gmail.com>
2022-01-31 13:06:22 +0600

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,16 @@
+$('.tutorial_slider').slick({
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+$('.clint_section_slider').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
